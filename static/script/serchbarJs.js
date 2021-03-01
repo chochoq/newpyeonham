93e36e31c2bfb2a50c82905320ca050ddd4fc3db
chochoq <aubepe@gmail.com>
2021-03-01 23:52:07 +0900

    modify searchbar js,css

--- static/script/serchbarJs.js
+++ static/script/serchbarJs.js
@@ -1,3 +1,8 @@
 function new_newsletter(){
-
+    let search_box =$('#search_insert')
+    if (search_box.css("display") === "block"){
+        search_box.hide();
+    }else {
+        search_box.show();
+    }
 }
