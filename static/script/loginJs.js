fbd30fc37af8e86706e0c66b7913f439ad25e46c
ouo_yoonk <joyk9251@gmail.com>
2021-03-01 20:52:25 +0900

    회원가입, 로그인 부분 html, js, css까지 대략입힘

--- static/script/loginJs.js
+++ static/script/loginJs.js
@@ -1,9 +1,26 @@
-const btn = document.querySelector('.signup_btn');
+const signup_btn = document.querySelector('.signup_btn');
+const login_btn = document.querySelector('.login_btn');
+const signup_section = document.querySelector('.signup_section');
+const login_section = document.querySelector('.login_section');
+
+const toggle_login_show = ()=>{
+    login_section.classList.toggle('show');
+}
+
+const toggle_signup_show = ()=>{
+    signup_section.classList.toggle('show');
+}
 
 signup_btn.addEventListener('click',()=>{
-    console.log('gg')
+    if(login_section.classList.contains('show')){
+        toggle_login_show();
+    }       
+    toggle_signup_show();    
 })
 
-function gg(){
-    console.log('gg')
-}
+login_btn.addEventListener('click',()=>{    
+    if(signup_section.classList.contains('show')){
+        toggle_signup_show();
+    }       
+    toggle_login_show();
+})
