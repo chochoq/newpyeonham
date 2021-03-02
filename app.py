877c43ac14ea139717c9e57b16cc7406e27f5190
chochoq <aubepe@gmail.com>
2021-03-02 21:21:54 +0900

    modify app.py api

--- app.py
+++ app.py
@@ -10,22 +10,79 @@ db = client.dbsparta
 
 ## URL 별로 함수명이 같거나,
 ## route('/') 등의 주소가 같으면 안됩니다.
-
 @app.route('/')
 def home():
     return render_template('index.html')
 
 
 ## API 역할을 하는 부분
-@app.route('/review', methods=['POST'])
+# 회원가입
+@app.route('/index/signup', methods=['POST'])
 def write_review():
     sample_receive = request.form['sample_give']
     print(sample_receive)
     return jsonify({'msg': '이 요청은 POST!'})
 
 
-@app.route('/review', methods=['GET'])
-def read_reviews():
+# 로그인
+@app.route('/index/login', methods=['POST'])
+def login():
+    sample_receive = request.form['sample_give']
+    print(sample_receive)
+    return jsonify({'msg': '이 요청은 POST!'})
+
+
+# 뉴스레터 추가
+@app.route('/index/insert', methods=['POST'])
+def insert():
+    sample_receive = request.form['sample_give']
+    print(sample_receive)
+    return jsonify({'msg': '이 요청은 POST!'})
+
+
+# 좋아요 관심
+@app.route('/index/like', methods=['POST'])
+def like():
+    sample_receive = request.form['sample_give']
+    print(sample_receive)
+    return jsonify({'msg': '이 요청은 POST!'})
+
+
+# 구독페이지 숨김
+@app.route('/index/hide', methods=['POST'])
+def hide():
+    sample_receive = request.form['sample_give']
+    print(sample_receive)
+    return jsonify({'msg': '이 요청은 POST!'})
+
+
+# 코멘트
+@app.route('/index/comment', methods=['POST'])
+def comment():
+    sample_receive = request.form['sample_give']
+    print(sample_receive)
+    return jsonify({'msg': '이 요청은 POST!'})
+
+
+# 검색
+@app.route('/index/search', methods=['GET'])
+def search():
+    sample_receive = request.args.get('sample_give')
+    print(sample_receive)
+    return jsonify({'msg': '이 요청은 GET!'})
+
+
+# 카테고리분류
+@app.route('/index/category', methods=['GET'])
+def category():
+    sample_receive = request.args.get('sample_give')
+    print(sample_receive)
+    return jsonify({'msg': '이 요청은 GET!'})
+
+
+# 새로고침
+@app.route('/index/refresh', methods=['GET'])
+def refresh():
     sample_receive = request.args.get('sample_give')
     print(sample_receive)
     return jsonify({'msg': '이 요청은 GET!'})
@@ -33,4 +90,3 @@ def read_reviews():
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
-
