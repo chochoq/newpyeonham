eb3e6c8e1f2ce9093141ef9751aec7ecb2ae0d82
ouo_yoonk <joyk9251@gmail.com>
2021-03-03 21:30:02 +0900

    스크래핑2

--- scraping copy.py
+++ scraping copy.py
@@ -8,9 +8,10 @@ db = client.dbsparta
 
 driver = webdriver.Chrome('./chromedriver')
 
-url = "https://www.i-boss.co.kr/ab-6141-52815"
 
-driver.get(url)
+url2 = "https://m.blog.naver.com/yoyoland/221846052931"
+
+driver.get(url2)
 sleep(3)
 
 # 맨 밑까지 내리기
@@ -21,18 +22,12 @@ sleep(5)
 req = driver.page_source
 driver.quit()
 
-list = [];
-
 soup = BeautifulSoup(req, 'html.parser')
-#btitleList = soup.select("div.wrap_body_frame > div.wrap_body.text_align_left.finish_txt > h2 > b ")
-#btitleList = soup.select("#contentDetailContent > p > span > strong")
 
-btitleList = soup.select("#ABA-wrapper-box-set > div.ABA-view-body.ABA-article-contents > ul > li >a ")
+btitleList = soup.select("#SE-e0b12b07-8162-426b-807d-90417ba0c65e > div > div > div")
 
-desList = soup.select("#ABA-wrapper-box-set > div.ABA-view-body.ABA-article-contents > ul > li ")
-
-print(desList)
-for idx in range(len(btitleList)):
+print(btitleList)
+""" for idx in range(len(btitleList)):
     
     if (idx>=0 and idx<4):
         db.newsletters.insert_one({'title':btitleList[idx].text.replace(':',''), 'url':btitleList[idx]['href'], 'desc':desList[idx].text, 'category':'시사 경제' })
@@ -40,4 +35,4 @@ for idx in range(len(btitleList)):
         db.newsletters.insert_one({'title':btitleList[idx].text.replace(':',''), 'url':btitleList[idx]['href'], 'desc':desList[idx].text, 'category':'마케팅 트렌드' })
     elif  idx>=10:
         db.newsletters.insert_one({'title':btitleList[idx].text.replace(':',''), 'url':btitleList[idx]['href'], 'desc':desList[idx].text, 'category':'문학 매거진' })
-
+ """
