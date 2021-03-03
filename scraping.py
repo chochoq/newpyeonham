63cc28b85c1c72731cdc5390670da9e98fbc5c1c
ouo_yoonk <joyk9251@gmail.com>
2021-03-03 21:07:35 +0900

    scrapping용 프로그램

--- scraping.py
+++ scraping.py
@@ -1,44 +1,46 @@
 from bs4 import BeautifulSoup
 from selenium import webdriver
 from time import sleep
+from pymongo import MongoClient
 
+client = MongoClient('localhost', 27017)
+db = client.dbsparta
 
 driver = webdriver.Chrome('./chromedriver')
 
-url = "https://brunch.co.kr/@moneyfire/256"
+url = "https://www.i-boss.co.kr/ab-6141-52815"
+
 driver.get(url)
 sleep(3)
 
-driver.execute_script("window.scrollTo(0, 1000)")  # 1000픽셀만큼 내리기
-sleep(1)
+
 # 맨 밑까지 내리기
 driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-sleep(10)
+sleep(5)
 
 
 req = driver.page_source
 driver.quit()
 
+list = [];
+
 soup = BeautifulSoup(req, 'html.parser')
-images = soup.select(".tile_item._item ._image._listImage")
+#btitleList = soup.select("div.wrap_body_frame > div.wrap_body.text_align_left.finish_txt > h2 > b ")
+#btitleList = soup.select("#contentDetailContent > p > span > strong")
+
+btitleList = soup.select("#ABA-wrapper-box-set > div.ABA-view-body.ABA-article-contents > ul > li >a ")
+
+desList = soup.select("#ABA-wrapper-box-set > div.ABA-view-body.ABA-article-contents > ul > li ")
+
+print(desList)
+for idx in range(len(btitleList)):
+    
+    if (idx>=0 and idx<4):
+        db.newsletters.insert_one({'title':btitleList[idx].text.replace(':',''), 'url':btitleList[idx]['href'], 'desc':desList[idx].text, 'category':'시사 경제' })
+    elif  (idx>=4 and idx<10):
+        db.newsletters.insert_one({'title':btitleList[idx].text.replace(':',''), 'url':btitleList[idx]['href'], 'desc':desList[idx].text, 'category':'마케팅 트렌드' })
+    elif  idx>=10:
+        db.newsletters.insert_one({'title':btitleList[idx].text.replace(':',''), 'url':btitleList[idx]['href'], 'desc':desList[idx].text, 'category':'문학 매거진' })
 
 
-aTag = body > div.service_contents.article_contents.\#post_view.beyond_content > div.wrap_view_article.wrap_article.article_view_disable_selection > div.wrap_body_frame > div.wrap_body.text_align_left.finish_txt > h4:nth-child(15) > span > span > a
-
-
-body > div.service_contents.article_contents.\#post_view.beyond_content > div.wrap_view_article.wrap_article.article_view_disable_selection > div.wrap_body_frame > div.wrap_body.text_align_left.finish_txt > h2   :nth-child(9)
-body > div.service_contents.article_contents.\#post_view.beyond_content > div.wrap_view_article.wrap_article.article_view_disable_selection > div.wrap_body_frame > div.wrap_body.text_align_left.finish_txt > h2:nth-child(14)
-
-
-body > div.service_contents.article_contents.\#post_view.beyond_content > div.wrap_view_article.wrap_article.article_view_disable_selection > div.wrap_body_frame > div.wrap_body.text_align_left.finish_txt > p:nth-child(10) > a
-body > div.service_contents.article_contents.\#post_view.beyond_content > div.wrap_view_article.wrap_article.article_view_disable_selection > div.wrap_body_frame > div.wrap_body.text_align_left.finish_txt > p:nth-child(11) > span > span
-body > div.service_contents.article_contents.\#post_view.beyond_content > div.wrap_view_article.wrap_article.article_view_disable_selection > div.wrap_body_frame > div.wrap_body.text_align_left.finish_txt > p:nth-child(12)
-
-
-
-print(len(images))
-
-for image in images:
-    src = image["src"]
-    print(src)
 
