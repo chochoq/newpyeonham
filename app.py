b81b7318c4a8835ee1560309a6ca6b28734adb69
chochoq <aubepe@gmail.com>
2021-03-03 11:31:54 +0900

    search ok

--- app.py
+++ app.py
@@ -4,10 +4,11 @@ import jwt
 import datetime
 import hashlib
 
-app = Flask(__name__)
-
 import requests
 from bs4 import BeautifulSoup
+
+app = Flask(__name__)
+
 
 from pymongo import MongoClient
 
@@ -36,30 +37,31 @@ def signup():
 
     return jsonify({'result': 'success'})
 
+
 @app.route('/index/insertSample', methods=['POST'])
 def insertSample():
     letter1 = {
-        'title' : 'ㅇㅎ! 아하레터',
-        'url' : 'https://page.stibee.com/subscriptions/61765?groupIds=56635',
-        'category' : '자기계발',
-        'img' : 'https://s3.ap-northeast-2.amazonaws.com/img.stibee.com/26042_list_61765_header_image.jpg?v=1598419760',
-        'desc' : '작심삼일 반복하면 못할것이 없습니다'
+        'title': 'ㅇㅎ! 아하레터',
+        'url': 'https://page.stibee.com/subscriptions/61765?groupIds=56635',
+        'category': '자기계발',
+        'img': 'https://s3.ap-northeast-2.amazonaws.com/img.stibee.com/26042_list_61765_header_image.jpg?v=1598419760',
+        'desc': '작심삼일 반복하면 못할것이 없습니다'
     }
 
     letter2 = {
-        'title' : '그랩의 IT 뉴스레터',
-        'url' : 'https://maily.so/grabnews',
-        'category' : 'IT',
-        'img' : 'https://cdn.maily.so/maily66df3af8fbfb998cda1caa2f235e7e8f1600609966',
-        'desc' : '매주 월요일, IT 콘텐츠 큐레이션 & 잘 읽히는 IT 개발지식을 제공합니다.'
+        'title': '그랩의 IT 뉴스레터',
+        'url': 'https://maily.so/grabnews',
+        'category': 'IT',
+        'img': 'https://cdn.maily.so/maily66df3af8fbfb998cda1caa2f235e7e8f1600609966',
+        'desc': '매주 월요일, IT 콘텐츠 큐레이션 & 잘 읽히는 IT 개발지식을 제공합니다.'
     }
-    
+
     letter3 = {
-        'title' : 'newneek',
-        'url' : 'https://newneek.co/?utm_medium=newsletter&utm_source=newneek&utm_campaign=dec21',
-        'category' : '종합',
-        'img' : 'https://newneek.co/static/media/gosum-home.7b7f5b6b.png',
-        'desc' : '월/수/금 아침마다 세상 돌아가는 소식을 메일로 받아보세요'
+        'title': 'newneek',
+        'url': 'https://newneek.co/?utm_medium=newsletter&utm_source=newneek&utm_campaign=dec21',
+        'category': '종합',
+        'img': 'https://newneek.co/static/media/gosum-home.7b7f5b6b.png',
+        'desc': '월/수/금 아침마다 세상 돌아가는 소식을 메일로 받아보세요'
     }
 
     db.newsletters.insert_one(letter1)
@@ -67,6 +69,7 @@ def insertSample():
     db.newsletters.insert_one(letter3)
 
     return jsonify({'result': 'success'})
+
 
 # 로그인
 SECRET_KEY = 'WECANDOANYTHING'
@@ -78,14 +81,12 @@ def login():
     print(sample_receive)
     return jsonify({'msg': '이 요청은 POST!'})
 
-
-# 뉴스레터 추가
 @app.route('/index/insert', methods=['POST'])
-def insert_newsletter():
-    title_receive = request.form['title_give']
-    category_receive = request.form['category_give']
+def post_articles():
     url_receive = request.form['url_give']
+    title_receive = request.form['title_give']
     desc_receive = request.form['desc_give']
+    category_receive = request.form['category_give']
 
     headers = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
@@ -102,13 +103,14 @@ def insert_newsletter():
         'image': image,
         'desc': desc,
         'url': url_receive,
-        'title': title_receive,
-        'category': category_receive,
-        'desc_newsletter': desc_receive
+        'title_news': title_receive,
+        'desc_news': desc_receive,
+        'category_news': category_receive
     }
-    db.newsletters.insert_one(doc)
 
-    return jsonify({'msg': '뉴스레터 완료되었습니다'})
+    db.newsletters.insert_one(doc)
+    return jsonify({'msg': '저장완료'})
+
 
 # 검색
 @app.route('/index/search', methods=['GET'])
@@ -116,6 +118,7 @@ def show_stars():
     sample_receive = request.args.get('sample_give')
     print(sample_receive)
     return jsonify({'msg': 'list 연결되었습니다!'})
+
 
 # 좋아요 관심
 @app.route('/index/like', methods=['POST'])
@@ -156,10 +159,12 @@ def refresh():
     print(sample_receive)
     return jsonify({'msg': '이 요청은 GET!'})
 
+
 def delete_letter():
     title_receive = request.form['title_give']
     db.letters.delete_one({'title': title_receive})
     return jsonify({'msg': '삭제'})
+
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
